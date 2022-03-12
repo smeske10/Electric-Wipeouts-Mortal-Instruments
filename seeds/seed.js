b65515ed60d634e73916431e3f67578993148577
brittany klose <brittany.klose@yahoo.com>
2022-03-12 14:21:09 -0500

    added in a cart.json

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,10 @@
 const sequelize = require("../config/connection");
-const { User, Product, Category} = require("../models");
+const { User, Product, Category, Cart} = require("../models");
 
 const userData = require("./userData.json");
 const productData = require("./productData.json");
 const categoryData = require("./categoryData.json");
+const cartData = require("./cartData.json");
 
 
 const seedDatabase = async () => {
@@ -15,6 +16,7 @@ const seedDatabase = async () => {
     returning: true,
   });
   await Product.bulkCreate(productData);
+  await Cart.bulkCreate(cartData);
 
   process.exit(0);
 };
