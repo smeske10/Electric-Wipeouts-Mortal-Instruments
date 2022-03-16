7b44e79230942414af60c09cf7cea6e70fe6390f
Adolfo Gonzalez <agonzal177@gmail.com>
2022-03-16 19:53:54 -0400

    delete btn

--- public/js/addToCart/cartBtn.js
+++ public/js/addToCart/cartBtn.js
@@ -1,23 +1,26 @@
-const cartBtn = document.querySelector("#test");
+const cartBtn = document.querySelector('#addBtn')
 
-const test = async (event) => {
-  if (event.target.hasAttribute("data-id")) {
-    let prod_id = event.target.getAttribute("data-id");
-    const response = await fetch(`/api/cart`, {
-      method: "POST",
-      body: JSON.stringify({ prod_id }),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    });
-    console.log("Response: " + response);
-    if (response.ok) {
-      document.location.replace("/cart");
-    } else {
-      alert("Failed to add to cart");
+const addToCart = async (event) => {
+
+    if (event.target.hasAttribute('data-id')) {
+        let prod_id = event.target.getAttribute('data-id');
+        console.log('In cartBtn')
+        console.log(prod_id)
+        const response = await fetch(`/api/cart`, {
+            method: 'POST',
+            body: JSON.stringify({ prod_id }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.ok) {
+            document.location.replace('/cart');
+        } else {
+            alert('Failed to add to cart');
+        }
     }
-  }
 };
-
-cartBtn.addEventListener("click", test);
+if(cartBtn){
+    cartBtn.addEventListener('click', addToCart)
+}
