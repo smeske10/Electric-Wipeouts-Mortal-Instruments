9bd5396da32b550cae548b0b7856e38e9cb5e191
Adolfo Gonzalez <agonzal177@gmail.com>
2022-03-17 19:08:05 -0400

    temp comment out

--- public/js/payment/checkoutBtn.js
+++ public/js/payment/checkoutBtn.js
@@ -1,22 +1,22 @@
-import nodeMail from "../nodemailer/nodemailer";
-import PaymentAPIWrapper from "./payment-api";
+// import nodeMail from "../nodemailer/nodemailer";
+// import PaymentAPIWrapper from "./payment-api";
 
-const checkoutBtn = document.querySelector("#checkout");
+// const checkoutBtn = document.querySelector("#checkout");
 
-const checkout = async (event) => {
-  const response = await fetch(`/checkout`, {
-    method: "GET",
-  });
-  if (response.ok) {
-    const payment = new PaymentAPIWrapper();
-  } else {
-    alert("Failed to checkout");
-  }
-  if (payment) {
-    nodeMail;
-  }
-};
+// const checkout = async (event) => {
+//   const response = await fetch(`/checkout`, {
+//     method: "GET",
+//   });
+//   if (response.ok) {
+//     const payment = new PaymentAPIWrapper();
+//   } else {
+//     alert("Failed to checkout");
+//   }
+//   if (payment) {
+//     nodeMail;
+//   }
+// };
 
-if (checkoutBtn) {
-  checkoutBtn.addEventListener("click", checkout);
-}
+// if (checkoutBtn) {
+//   checkoutBtn.addEventListener("click", checkout);
+// }
